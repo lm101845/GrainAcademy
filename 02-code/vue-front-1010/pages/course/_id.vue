4a4a7d2b19f69b105bc8fbd21bf3f1659f021ff3
lm101845 <1018457683@qq.com>
2023-06-19 23:11:34 +0800

    第231-236集看完

--- 02-code/vue-front-1010/pages/course/_id.vue
+++ 02-code/vue-front-1010/pages/course/_id.vue
@@ -16,7 +16,7 @@
       <div>
         <article class="c-v-pic-wrap" style="height: 357px;">
           <section class="p-h-video-box" id="videoPlay">
-            <img :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
+            <img height="357px" :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
           </section>
         </article>
         <aside class="c-attr-wrap">
@@ -38,12 +38,12 @@
               </span>
             </section>
             <section class="c-attr-mt">
-              <a href="#" title="立即观看" class="comm-btn c-btn-3">立即观看</a>
+              <a @click="createOrders()" href="#" title="立即购买" class="comm-btn c-btn-3">立即购买</a>
             </section>
           </section>
         </aside>
         <aside class="thr-attr-box">
-          <ol class="thr-attr-ol clearfix">
+          <ol class="thr-attr-ol">
             <li>
               <p>&nbsp;</p>
               <aside>
@@ -166,18 +166,32 @@
 
 <script>
 import courseApi from '@/api/course'
+import ordersApi from '@/api/orders'
 export default {
   asyncData({ params, error }) {
     return courseApi.getCourseInfo(params.id)
       .then(response => {
-        console.log(response,'getCourseInfo--response')
         return {
           courseWebVo: response.data.data.courseWebVo,
-          chapterVideoList: response.data.data.chapterVideoList
+          chapterVideoList: response.data.data.chapterVideoList,
+          courseId:params.id
         }
       })
+  },
+  methods:{
+    //生成订单
+    createOrders() {
+      ordersApi.createOrders(this.courseId)
+        .then(response => {
+          console.log(response,'生成订单')
+          //获取返回订单号
+          //生成订单之后，跳转订单显示页面
+          this.$router.push({path:'/orders/'+response.data.data.orderNo})
+        })
+    }
   }
 };
 </script>
+
 
 
